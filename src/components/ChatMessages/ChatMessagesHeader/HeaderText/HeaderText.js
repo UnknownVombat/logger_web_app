c98099879c444deb18d5b2af4dd8da6033e71322
SukaNahuiBlyat <dvadva92@inbox.ru>
2023-11-16 21:37:43 +0300

    version 0.3

--- src/components/ChatMessages/ChatMessagesHeader/HeaderText/HeaderText.js
+++ src/components/ChatMessages/ChatMessagesHeader/HeaderText/HeaderText.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from "./HeaderText.module.css";
 import {messages} from "../../../../Array";
 
-const HeaderText = (chat_id) => {
+const HeaderText = ({chat_id}) => {
     return (
         <div className={styles.textDiv}>
             <h3 className={styles.text}>Диалог с пользователем {messages[chat_id].name}</h3>
