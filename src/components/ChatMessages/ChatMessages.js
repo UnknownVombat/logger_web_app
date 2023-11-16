1609d6fc23d305c0a865406c5d1548c1608922b2
SukaNahuiBlyat <dvadva92@inbox.ru>
2023-11-16 19:33:43 +0300

    version 0.2

--- src/components/ChatMessages/ChatMessages.js
+++ src/components/ChatMessages/ChatMessages.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import {useParams} from "react-router-dom";
+import {Link, useParams} from "react-router-dom";
 import styles from './ChatMessages.module.css'
 import Message from './MessagesDiv/MessagesDiv'
-import ChatsMessagesHeader from "./ChatsMessagesHeader";
+import {messages} from "../../Array";
 
 const ChatMessages = () => {
     const {chat_id} = useParams();
     try{return (
         <div className={styles.ChatMessagesDiv}>
-            <ChatsMessagesHeader chat_id={chat_id}/>
+            <Link to={'/'}>
+                <div>
+                    <h3 className={styles.h3}>Диалог с пользователем {messages[chat_id].name}</h3>
+                    <h2 className={styles.h2}>Возврат к чатам</h2>
+                </div>
+            </Link>
             <Message chat_id={chat_id}/>
         </div>
     )} catch {return (<div className={styles.h3}>Чат не найден</div>)}
