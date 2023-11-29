c26bc461e5b00be2353ee6ec26523bb49e4b7e8e
SukaNahuiBlyat <dvadva92@inbox.ru>
2023-11-29 20:30:46 +0300

    version 1.2 with хуй

--- src/components/ChatMessages/MessagesDiv/MessagesDiv.js
+++ src/components/ChatMessages/MessagesDiv/MessagesDiv.js
@@ -5,6 +5,7 @@ import {messageStorage} from "../../../storages/MessagesStorage";
 
 const MessagesDiv = () => {
     const messages = messageStorage((state) => state.messages)
+    alert(JSON.stringify(messages));
     if (messages.length > 0){
         return (
             <div className={styles.MessagesDiv}>
