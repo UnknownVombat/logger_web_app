eeb2fd1cdedcb14572e413a4dd6938dee3df63a7
SukaNahuiBlyat <dvadva92@inbox.ru>
2023-11-29 21:09:41 +0300

    version 1.3 without хуй (мы пофиксили)

--- src/components/ChatsList/ChatsList.js
+++ src/components/ChatsList/ChatsList.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import styles from './ChatsList.module.css'
 import ChatCard from "./ChatCard/ChatCard";
 import {chatStorage} from "../../storages/ChatSearchStorage";
