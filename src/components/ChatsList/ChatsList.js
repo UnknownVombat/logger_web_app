427a71cc4a1e8dfde4d0a6b23f905bb9f1a5c7dd
SukaNahuiBlyat <dvadva92@inbox.ru>
2023-11-30 22:53:05 +0300

    version 1.3.2 делаем ебаный рендер в хедере

--- src/components/ChatsList/ChatsList.js
+++ src/components/ChatsList/ChatsList.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import styles from './ChatsList.module.css'
 import ChatCard from "./ChatCard/ChatCard";
 import {chatStorage} from "../../storages/ChatSearchStorage";
+import {messageStorage} from "../../storages/MessagesStorage";
+import {getChats} from "../../Requests";
 
 const ChatsList = () => {
     // const user_id = 530472030
+    const wa = window.Telegram.WebApp;
+    const user_id = wa.initDataUnsafe?.user?.id;
     const chats = chatStorage((state => state.filteredChats));
+    const setUser = messageStorage((state) => state.setUser);
+    const filterChats = chatStorage((state) => state.filterChats)
+    const setChats = chatStorage((state) => state.setChats);
+    useEffect(() => {
+        async function parseChats(user_id){
+            const chatsList = await getChats({user_id});
+            setChats(chatsList);
+            filterChats()
+        }
+
+        setUser({user_id})
+        parseChats(user_id)
+    }, [user_id, setChats, setUser, filterChats]);
     if (chats.length === 0) {
         return (<div className={styles.ChatDiv}>
                     <h3 className={styles.h3}>Чаты</h3>
                     <p>Идет загрузка</p>
                 </div>)
     }
+    window.location.reload()
     try {
         return (
             <div className={styles.ChatDiv}>
--- src/components/Header/AccountChoose/AccountChoose.js
+++ src/components/Header/AccountChoose/AccountChoose.js
@@ -1,8 +1,5 @@
-import React, {useEffect} from "react";
+import React from "react";
 import styles from './AccountChoose.module.css';
-import {getChats} from "../../../Requests";
-import {chatStorage} from "../../../storages/ChatSearchStorage";
-import {messageStorage} from "../../../storages/MessagesStorage";
 
 
 const AccountChoose = () => {
@@ -10,21 +7,6 @@ const AccountChoose = () => {
     const user_id = wa.initDataUnsafe?.user?.id;
     const username = wa.initDataUnsafe?.user?.username;
     const accs = [username, user_id];
-    const setUser = messageStorage((state) => state.setUser);
-    const filterChats = chatStorage((state) => state.filterChats)
-    const setChats = chatStorage((state) => state.setChats);
-    useEffect(() => {
-        async function parseChats(user_id){
-            const chatsList = await getChats({user_id});
-            setChats(chatsList);
-            filterChats()
-        }
-
-        setUser({user_id})
-        parseChats(user_id)
-    }, [user_id, setChats, setUser, filterChats]);
-
-    window.location.reload()
     return(<div className={styles.SelectorDiv}>
         Чаты аккаунта:
         <select className={styles.SelectorSelect} >
