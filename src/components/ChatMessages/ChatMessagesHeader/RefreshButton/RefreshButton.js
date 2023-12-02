ad247a5e4158d3d0d48991c7f5a60e8a2e862f06
SukaNahuiBlyat <dvadva92@inbox.ru>
2023-12-03 02:44:28 +0300

    version 1.3.4 ща ебанет

--- src/components/ChatMessages/ChatMessagesHeader/RefreshButton/RefreshButton.js
+++ src/components/ChatMessages/ChatMessagesHeader/RefreshButton/RefreshButton.js
@@ -4,6 +4,7 @@ import {NavLink} from "react-router-dom";
 
 
 const RefreshButton = ({props}) => {
+    console.log(props)
     return (
         <NavLink to={`/messages/${props[0]}/${props[1]}`} className={styles.CardLink}>
             <box-icon name='refresh' color='var(--tg-theme-text-color)'></box-icon>
