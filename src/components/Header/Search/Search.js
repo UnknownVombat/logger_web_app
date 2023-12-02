db7d11f444e8a763651c908d68cb8cbbf8d45e78
SukaNahuiBlyat <dvadva92@inbox.ru>
2023-12-02 22:53:10 +0300

    version 1.3.3 фикс фильтрации

--- src/components/Header/Search/Search.js
+++ src/components/Header/Search/Search.js
@@ -7,16 +7,18 @@ const Search = () => {
     const placeText = chatStorage((state) => state.searchText)
     const changeText = chatStorage((state) => state.changeText)
     const filterChats = chatStorage((state) => state.filterChats)
-    useEffect(() => {
+    try {useEffect(() => {
         filterChats()
-    }, [placeText, filterChats]);
-    try{
-        return(<div className={styles.SearchDiv}>
-        <input type='text' placeholder={placeText} className={styles.SearchInput} onInput={(event) => changeText(event.target.value.toLowerCase())}/>
-    </div>)
-    } catch (error) {
+    }, [placeText, filterChats]);}
+    catch (error) {
         alert(error)
     }
+    return(
+        <div className={styles.SearchDiv}>
+            <input type='text' placeholder={placeText} className={styles.SearchInput} onInput={(event) => changeText(event.target.value.toLowerCase())}/>
+        </div>
+    )
+
 
 }
 
