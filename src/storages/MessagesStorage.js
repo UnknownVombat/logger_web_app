79367c8b06d69d5750ef2c436b46cbe69ac57c4d
SukaNahuiBlyat <dvadva92@inbox.ru>
2023-12-02 23:45:05 +0300

    version 1.3.3 фикс фильтрации

--- src/storages/MessagesStorage.js
+++ src/storages/MessagesStorage.js
@@ -8,10 +8,9 @@ export const messageStorage = create(persist(
         chat_id: '0',
         name: '0',
         messages: [],
-        setMessages: (mess) => set((state) => ({messages: mess})),
-        setName: (name) => set((state) => ({name: name})),
-        setUser: (user_id) => set((state) => ({user_id: user_id})),
-        revokeMessages: (id, name) => set((state) => ({chat_id: id, name: name}))
+        setMessages: (mess) => set(() => ({messages: mess})),
+        setUser: (user_id) => set(() => ({user_id: user_id})),
+        revokeMessages: (id, name) => set(() => ({chat_id: id, name: name}))
     }),
     {name: 'chat-messages-storage',
         storage: createJSONStorage(() => sessionStorage)}
