d984e7c2df5450a416491ee69852e973339ec592
SukaNahuiBlyat <dvadva92@inbox.ru>
2023-12-03 02:24:23 +0300

    version 1.3.4 ща ебанет

--- src/components/ChatMessages/ChatMessagesHeader/RefreshButton/RefreshButton.js
+++ src/components/ChatMessages/ChatMessagesHeader/RefreshButton/RefreshButton.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import styles from "./RefreshButton.module.css";
+import {getMessages} from "../../../../Requests";
 
 
-function makeRequest(chat_id){
-    console.log('Делаем запрос для чата', chat_id)
-}
+// function makeRequest(chat_id){
+//     console.log('Делаем запрос для чата', chat_id)
+// }
 
 const RefreshButton = (chat_id) => {
+    const user_id = window.Telegram.WebApp.initDataUnsafe?.user?.id
     return (
-        <div className={styles.buttonDiv} onClick={() => makeRequest(chat_id.chat_id)}>
+        <div className={styles.buttonDiv} onClick={() => getMessages(user_id, chat_id.chat_id)}>
             <box-icon name='refresh' color='var(--tg-theme-text-color)'></box-icon>
         </div>
     );
