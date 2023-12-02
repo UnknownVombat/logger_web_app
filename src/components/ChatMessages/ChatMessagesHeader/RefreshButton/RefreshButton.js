6311c67e23b84954c298dcba763025d89f3509f0
SukaNahuiBlyat <dvadva92@inbox.ru>
2023-12-03 02:51:16 +0300

    version 1.3.4 ща ебанет

--- src/components/ChatMessages/ChatMessagesHeader/RefreshButton/RefreshButton.js
+++ src/components/ChatMessages/ChatMessagesHeader/RefreshButton/RefreshButton.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import styles from "./RefreshButton.module.css";
 import {NavLink} from "react-router-dom";
+import {getMessages} from "../../../../Requests";
 
 
 const RefreshButton = ({chat_id}) => {
-    window.location.reload()
+    const user_id = window.Telegram.WebApp.initDataUnsafe?.user?.id
     return (
-        <NavLink to={`/messages/${chat_id[0]}/${chat_id[1]}`} className={styles.CardLink}>
+        <NavLink to={`/messages/${chat_id[0]}/${chat_id[1]}`} className={styles.CardLink} onClick={() => getMessages(user_id, chat_id[0])}>
             <box-icon name='refresh' color='var(--tg-theme-text-color)'></box-icon>
         </NavLink>
     );
