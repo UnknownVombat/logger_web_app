8c06ac50978c52de498b0254a556b07aaa85a79f
SukaNahuiBlyat <dvadva92@inbox.ru>
2023-12-02 23:59:15 +0300

    version 1.3.4 design fixes

--- src/components/ChatMessages/MessageCard/MessageCard.js
+++ src/components/ChatMessages/MessageCard/MessageCard.js
@@ -6,11 +6,14 @@ const MessageCard = (message) => {
     const user_id = messageStorage((state) => state.user_id)
     const from = (message.senderId === user_id ? 'user': 'opponent')
     const cl = {'user': styles.MessageDivUser, 'opponent': styles.MessageDivOpponent}
+    const date = new Date(message.time)
+    const newDate = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds() + ' ' + date.getDay() +
+        '.' + date.getMonth() + '.' + date.getFullYear()
     return (
         <div className={cl[from]} key={message.id}>
             {message?.img ? <img src={message.img} alt=''/>: null}
             {message?.text ? <p>{message.text}</p>: null}
-            <p>{message.time}</p>
+            <p className={styles.time}>{newDate}</p>
         </div>
     );
 };
--- src/components/ChatsList/ChatCard/ChatInfo/ChatInfo.js
+++ src/components/ChatsList/ChatCard/ChatInfo/ChatInfo.js
@@ -2,12 +2,15 @@ import React from 'react';
 import styles from "./ChatInfo.module.css";
 
 const ChatInfo = ({chat}) => {
+    const date = new Date(chat.lastMessage)
+    const newDate = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds() + ' ' + date.getDay() +
+        '.' + date.getMonth() + '.' + date.getFullYear()
     return (
         <div key={chat.chat_id} className={styles.CardDiv}>
             <p>Пользователь: {chat.name}</p>
             <p>TelegramID: {chat.telegramId}</p>
             <p>Username: {chat.username}</p>
-            <p>Last recent message: {chat.lastMessage}</p>
+            <p>Last recent message: {newDate}</p>
         </div>
     );
 };
